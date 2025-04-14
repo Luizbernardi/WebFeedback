6f76450998d1cc767b0020adf880ace96eac6cf5
LuizBernardi <luizbernardi37@gmail.com>
2025-04-14 20:23:23 -0300

    ajustando rotas para bruno

--- feedback/views.py
+++ feedback/views.py
@@ -7,17 +7,17 @@ class HomeView(View):
     def index(request):
         if request.user.is_authenticated:  
             disciplinas = Disciplina.objects.all()  
-            return render(request, 'feedback/pages/lista-disciplina.html', {'disciplinas': disciplinas})
+            return render(request, 'pages/lista-disciplina.html', {'disciplinas': disciplinas})
         else:
             disciplinas = Disciplina.objects.all()  
-            return render(request, 'feedback/pages/home.html', {'disciplinas': disciplinas})
+            return render(request, 'pages/home.html', {'disciplinas': disciplinas})
         
 @login_required(login_url='login')
 def disciplina_list_view(request):
     disciplinas = Disciplina.objects.all()  
-    return render(request, 'feedback/pages/lista-disciplina.html', {'disciplinas': disciplinas})
+    return render(request, 'pages/lista-disciplina.html', {'disciplinas': disciplinas})
 
 @login_required(login_url='login')
 def disciplina_detalhes_view(request, pk):
     disciplinas = Disciplina.objects.get(pk=pk)
-    return render(request, 'feedback/pages/detalhe-disciplina.html', {'disciplinas': disciplinas})
+    return render(request, 'pages/detalhe-disciplina.html', {'disciplinas': disciplinas})
